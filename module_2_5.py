0b4ab2736d1ed6ad3017b89ac6353d21a345c035
Godzdor8 <145772383+Godzdor8@users.noreply.github.com>
2024-10-05 17:07:29 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,16 @@
+def get_matrix(n, m , value):
+
+    matrix = []
+
+    if n <= 0 or m <= 0:
+        return matrix
+
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+
+print(get_matrix(2,2,10))
+print(get_matrix(3,5,42))
+print(get_matrix(4,2,13))
